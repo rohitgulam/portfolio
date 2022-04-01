8e5c729e1d4c2fc03bd00df2a1bae14e78bcb72c
Rohit Gulam <rohitsgulam@gmail.com>
2022-04-01 18:41:05 +0300

    hero component

---
+++ src/componets/Hero.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+import HeroDescription from './HeroDescription';
+import HeroImg from './HeroImg';
+
+function Hero() {
+  return (
+    <Div>
+        <HeroDescription/>
+        <HeroImg/>
+    </Div>
+  )
+}
+
+const Div = styled.div`
+    margin: 50px 80px;
+    display: flex;
+    // align-items: center;
+    justify-content: space-between;
+    height: 88vh;
+`;
+
+export default Hero
