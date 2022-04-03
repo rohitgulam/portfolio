ba2cb5fcfd16d84bdeb328c1bf50c2532f0221a9
Rohit Gulam <rohitsgulam@gmail.com>
2022-04-03 17:01:02 +0300

    placehoder icon section

---
+++ src/componets/IconSection.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function IconSection() {
+  return (
+    <Div>
+        
+    </Div>
+  )
+}
+
+const Div = styled.div`
+    height: 20vh;
+    background-color: aliceblue;
+`;
+
+export default IconSection
