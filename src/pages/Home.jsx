45203d2315da51a16670328702939faef10dea28
Rohit Gulam <rohitsgulam@gmail.com>
2022-04-03 16:59:38 +0300

    reduced height to make alignment look good

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Hero from '../componets/Hero';
 import Service from '../componets/Service';
+import IconSection from '../componets/IconSection';
 
 function Home() {
   return (
     <>
       <Hero/>
+      <IconSection/>
       <Service/>
     </>
   )
