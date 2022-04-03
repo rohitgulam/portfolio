f935a653b5ed52cd72b1f200fbec66eafe522fae
Rohit Gulam <rohitsgulam@gmail.com>
2022-04-03 15:49:55 +0300

    adjsuted width

--- src/componets/Hero.jsx
+++ src/componets/Hero.jsx
@@ -14,7 +14,7 @@ function Hero() {
 
 const Div = styled.div`
     margin: 50px auto;
-    width: 70%;
+    width: 80%;
     display: flex;
     // align-items: center;
     justify-content: space-between;
